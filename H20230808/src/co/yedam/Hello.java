5ab3797e765444127195ba58485082d80e5dab87
ohahrin <141694719+ohahrin@users.noreply.github.com>
2023-08-08 11:35:34 +0900

    Update Hello.java

--- H20230808/src/co/yedam/Hello.java
+++ H20230808/src/co/yedam/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요...");
 		System.out.println("remote에 반영하기 전");
+		System.out.println("원격에 작업을 반영함");
 	}
 }
